e0ad3d613bc5d9e31ac7921cdf004ac0944af4f9
zacharykatz1 <zacharykatz1@gmail.com>
2023-02-13 20:14:32 -0500

    Updated shell and R scripts

--- practicum_shell_script.sh
+++ practicum_shell_script.sh
@@ -2,8 +2,9 @@
 #$ -cwd -l mem=3g,time=01:00:00 -S /bin/bash -N JOBa1 -j y -t 1-10
 
 currind=$SGE_TASK_ID
+module load R/4.2.2
 
-R=/nfs/apps/R/4.2.2/bin/R
-export R_LIBS_USER=/ifs/home/msph/biostat/zak2132/R_LIB:/ifs/scratch/msph/software/R/library422:/ifs/scratch/msph/software/R/library:$R_LIBS_USER
+# R=/nfs/apps/R/4.2.2/bin/R
+export R_LIBS_USER=/ifs/scratch/msph/software/R/library422:/ifs/home/msph/biostat/zak2132/R_LIB:/ifs/scratch/msph/software/R/library:$R_LIBS_USER
 
-${R} --vanilla --args $currind < practicum_hplc_zk.R
+Rscript --vanilla --args $currind < practicum_hplc_zk.R
